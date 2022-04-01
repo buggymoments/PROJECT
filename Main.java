96c65a73b08958246cac1ca9ecfb92e88c838d7c
buggymoments <91684989+buggymoments@users.noreply.github.com>
2022-04-01 17:45:14 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,89 @@
+package buggymoments;
+import javax.swing.*;
+public class Main {
+    /*
+    Pwedeng switch to pero depende
+    Prices as of March 28, 2022
+    Hiwalay natin kunwari BitcoinConvert, tapos LiteCoinConvert, and ye you get the rest naman na
+    So yeah lets categorize this first
+    Bitcoin, Ethereum, Litecoin, Cardano, Binance Coin
+    1 BTC to Eth = 14.22
+    1 Eth to BTC = 0.070
+    1 BTC to LTC = 361
+    1 LTC to BTC = 0.0028
+    1 LTC to ETH = 0.04
+    1 Eth to LTC = 25.34
+    1 BTC to Cardano = 40067.79
+    1 Cardano (ADA) to BTC = 0.000025
+    1 ADA to ETh = 0.000355
+    1 Eth to ADA = 2818
+    1 ADA to LTC = 0.009
+    1 LTC to ADA = 110.84
+    1 BNB to BTC = 0.009162
+    1 BTC to BNB = 109.09
+    1 BNB to ETH = 0.130174
+    1 ETH to BNB = 7.68
+    1 BNB to LTC = 3.30966
+    1 LTC to BNB = 0.301937
+    1 BNB to ADA = 366.194908
+    1 ADA to BNB = 0.002729
+
+     */
+    /*
+
+            UI
+
+
+     */
+    public void BTCConvert(){
+        /* BTC to BTC
+           BTC to ETH
+           BTC to ADA
+           BTC to LTC
+           BTC to BNB
+         */
+    }
+
+    public void ETHConvert(){
+        /* ETH to ETH
+           ETH to BTC
+           ETH to ADA
+           ETH to LTC
+           ETH to BNB
+         */
+    }
+
+    public void LTCConvert(){
+        /* LTC to LTC
+           LTC to BTC
+           LTC to ETH
+           LTC to ADA
+           LTC to BNB
+         */
+    }
+
+    public void ADAConvert(){
+        /* ADA to ADA
+           ADA to BTC
+           ADA to ETH
+           ADA to LTC
+           ADA to BNB
+         */
+    }
+
+    public void BNBConvert(){
+        /* BNB to BNB
+           BNB to BTC
+           BNB to ETH
+           BNB to LTC
+           BNB to ADA
+         */
+    }
+    public static void main(String[] args) {
+
+    }
+
+
+        }
+
+
