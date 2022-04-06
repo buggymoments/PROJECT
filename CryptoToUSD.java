e1806537be2f76692f38458fcd1aba0184a940a9
buggymoments <91684989+buggymoments@users.noreply.github.com>
2022-04-06 11:06:22 +0800

    Create CryptoToUSD.java

---
+++ CryptoToUSD.java
@@ -0,0 +1,19 @@
+package buggymoments;
+
+public class Main {
+
+    //Crypto to USD converter
+
+    public void cryptoToUSD(){
+        /*  BTC to USD  1 = 45,296.70
+            ETH to USD  1 = 3,352.23
+            ADA to USD  1 = 1.14
+            LTC to USD  1 = 120.30
+            BNB to USD  1 = 441.92
+         */
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
