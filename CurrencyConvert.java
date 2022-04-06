1033995add32ff4098df30d0f683c68e5676cca2
buggymoments <91684989+buggymoments@users.noreply.github.com>
2022-04-06 11:00:35 +0800

    Update and rename CurrencyConvert to CurrencyConvert.java

---
+++ CurrencyConvert.java
@@ -0,0 +1,55 @@
+package buggymoments;
+
+public class Main {
+
+    // Currency Converter
+
+    public void USDConvert(){
+        /* USD to USD   1 = 1
+           USD to PHP   1 = 51
+           USD to GBP   1 = 0.77
+           USD to EUR   1 = 0.92
+           USD to CAD   1 = 1.25
+         */
+    }
+
+    public void PHPConvert(){
+        /* PHP to PHP   1 = 1
+           PHP to USD   1 = 0.019
+           PHP to GBP   1 = 0.015
+           PHP to EUR   1 = 0.018
+           PHP to CAD   1 = 0.024
+         */
+    }
+
+    public void GBPConvert(){
+        /* GBP to GBP   1 = 1
+           GBP to USD   1 = 1.31
+           GBP to PHP   1 = 67.16
+           GBP to EUR   1 = 1.20
+           GBP to CAD   1 = 1.63
+         */
+    }
+
+    public void EuroConvert(){
+        /* EUR to EUR   1 = 1
+           EUR to USD   1 = 1.09
+           EUR to PHP   1 = 55.98
+           EUR to GBP   1 = 0.83
+           EUR to CAD   1 = 1.36
+         */
+    }
+
+    public void CADConvert(){
+        /* CAD to CAD   1 = 1
+           CAD to USD   1 = 0.80
+           CAD to PHP   1 = 41.12
+           CAD to GBP   1 = 0.61
+           CAD to EUR   1 = 0.73
+         */
+    }
+    
+    public static void main(String[] args) {
+    
+    }
+}
