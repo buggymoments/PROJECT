b531935789daf876dc68180ed72d25e5fb044930
buggymoments <91684989+buggymoments@users.noreply.github.com>
2022-04-11 10:53:10 +0800

    Update and rename Main.java to CryptoConverter.java

--- CryptoConverter.java
+++ CryptoConverter.java
@@ -2,7 +2,6 @@ package buggymoments;
 import javax.swing.*;
 public class Main {
     /*
-    Pwedeng switch to pero depende
     Prices as of March 28, 2022
     Hiwalay natin kunwari BitcoinConvert, tapos LiteCoinConvert, and ye you get the rest naman na
     So yeah lets categorize this first
@@ -83,63 +82,6 @@ public class Main {
          */
     }
 
-    //Crypto to USD converter
-
-    public void cryptoToUSD(){
-        /*  BTC to USD  1 = 45,296.70
-            ETH to USD  1 = 3,352.23
-            ADA to USD  1 = 1.14
-            LTC to USD  1 = 120.30
-            BNB to USD  1 = 441.92
-         */
-    }
-
-    // Currency Converter
-
-    public void USDConvert(){
-        /* USD to USD   1 = 1
-           USD to PHP   1 = 51
-           USD to GBP   1 = 0.77
-           USD to EUR   1 = 0.92
-           USD to CAD   1 = 1.25
-         */
-    }
-
-    public void PHPConvert(){
-        /* PHP to PHP   1 = 1
-           PHP to USD   1 = 0.019
-           PHP to GBP   1 = 0.015
-           PHP to EUR   1 = 0.018
-           PHP to CAD   1 = 0.024
-         */
-    }
-
-    public void GBPConvert(){
-        /* GBP to GBP   1 = 1
-           GBP to USD   1 = 1.31
-           GBP to PHP   1 = 67.16
-           GBP to EUR   1 = 1.20
-           GBP to CAD   1 = 1.63
-         */
-    }
-
-    public void EuroConvert(){
-        /* EUR to EUR   1 = 1
-           EUR to USD   1 = 1.09
-           EUR to PHP   1 = 55.98
-           EUR to GBP   1 = 0.83
-           EUR to CAD   1 = 1.36
-         */
-    }
-
-    public void CADConvert(){
-        /* CAD to CAD   1 = 1
-           CAD to USD   1 = 0.80
-           CAD to PHP   1 = 41.12
-           CAD to GBP   1 = 0.61
-           CAD to EUR   1 = 0.73
-         */
-    }
     
     public static void main(String[] args) {
 
